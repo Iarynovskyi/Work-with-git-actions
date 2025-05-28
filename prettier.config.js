38a8b35a098f0bbb3d565321101f89944e5253fb
Iarynovskyi <dimayar20061@gmail.com>
2025-05-28 15:28:32 +0300

    test pretier

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,10 @@
 // prettier.config.js
 module.exports = {
+  semi: false,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  printWidth: 120,
+  endOfLine: 'lf',
   singleQuote: true,
-  tabWidth: 2,
-};
+  tabWidth: 2
+}
