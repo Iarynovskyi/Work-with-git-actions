868243ef83315ec538fa994dc59653d88ab1c2f9
Iarynovskyi <dimayar20061@gmail.com>
2025-05-28 15:33:58 +0300

    test with some change

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,9 @@
 // prettier.config.js
 module.exports = {
-  semi: false,
   trailingComma: 'none',
   arrowParens: 'always',
   printWidth: 120,
   endOfLine: 'lf',
   singleQuote: true,
   tabWidth: 2
-}
+};
