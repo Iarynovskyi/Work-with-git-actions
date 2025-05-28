cbd8423235e893eee0a9c75d3a86e6c1a14db54a
Iarynovskyi <dimayar20061@gmail.com>
2025-05-29 00:07:02 +0300

    must break

---
+++ app/page.test.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Home from './page';
+
+describe('Test Home', () => {
+  it('renders the Home component correctly', () => {
+    render(<Home />);
+    const aboutElement = screen.getByText('Hell');
+    expect(aboutElement).toMatchSnapshot();
+  });
+});
