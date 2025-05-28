d5ea65bb020d4ce6a0fb46431c30e8c5fa0a64ac
Iarynovskyi <dimayar20061@gmail.com>
2025-05-28 15:32:14 +0300

    test one more space

--- app/test-page/page.tsx
+++ app/test-page/page.tsx
@@ -1,10 +1,3 @@
-
-
-
-export default    function Page() {
-
-  console.log('qwqw');
-
-
-  return     <h1>        Hello, Next.js!           </h1>;
+export default function Page() {
+  return <h1> Hello, Next.js! </h1>
 }
