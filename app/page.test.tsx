4b5048e31c529adb6ee14749a8db497af3e0c8b9
Iarynovskyi <dimayar20061@gmail.com>
2025-06-09 20:38:41 +0300

    feat: eslint

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,9 +1,10 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+
 import Home from './page';
 
 it('renders the Home component correctly', async () => {
   render(<Home />);
-  const aboutElement = await screen.findByText(/Hello/i)
+  const aboutElement = await screen.findByText(/Hello/i);
   expect(aboutElement).toBeInTheDocument();
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import '../commitlint.config';
+
 export default function Home() {
-  return <h1> Hello </h1>;
+  const a = 0 ? 1 : 0;
+  return <h1> Hello {a}</h1>;
 }
